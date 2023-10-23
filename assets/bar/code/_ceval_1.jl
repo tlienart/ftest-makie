461c35ede430ad4a77ce06c4c52f403eaf30ba4d
tlienart <tlienart@me.com>
2023-10-23 07:20:21 +0000

    Deploying to gh-pages from  @ 058bf09ace9cd3eff4e91f293936f4d8ba383783 🚀

--- assets/bar/code/_ceval_1.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-using Makie.LaTeXStrings
-
-L"$\alpha$"
---
+++ assets/index/code/_ceval_1.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+using GMT, Printf
+@sprintf "this is a %s %15.1f" "test" 34.567
